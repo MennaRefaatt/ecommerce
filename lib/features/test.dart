142448566bd6c76f4a59e28e89c6b965ee39a463
Mennorefaat <bmennarefaat84@gmail.com>
2024-10-13 11:36:42 +0300

    last edit

--- lib/features/test.dart
+++ lib/features/test.dart
@@ -2,11 +2,9 @@ import 'package:ecommerce/core/helpers/safe_print.dart';
 import 'package:ecommerce/core/helpers/secure_storage/secure_storage.dart';
 import 'package:ecommerce/core/services/navigation/app_endpoints.dart';
 import 'package:ecommerce/core/utils/app_image.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../core/helpers/secure_storage/secure_keys.dart';
 
 class Test extends StatefulWidget {
@@ -37,6 +35,7 @@ class _TestState extends State<Test> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
+
           child: AppImage(
         path: "app_icon2.png",
         width: 100.w,
